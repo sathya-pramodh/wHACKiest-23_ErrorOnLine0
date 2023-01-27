40f10b3ea184007b5e4c32b66102d1d01113bf36
sathya-pramodh <sathyapramodh17@gmail.com>
2023-01-27 20:22:24 +0530

    frontend: added boiler plate code.

---
+++ travelmon/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+const App = () => {
+    return (
+        <HomePage />
+    )
+};
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
+export default App;
---
+++ travelmon/frontend/src/components/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+const HomePage = () => {
+    return (
+        <div>
+            <h1>This is the home page.</h1>
+        </div>
+    )
+};
+export default HomePage;
